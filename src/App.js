d76cd1d64f3ef3003a5576b217251173e16846d8
jobvillagran <jobvillagran@gmail.com>
2022-05-03 10:17:54 -0600

    work 03-05-22

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Welcome from './components/Welcome';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Contact from './pages/Contact';
+import About from './pages/About';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Router>
+          <div>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/contact">Contacto</Link></li>
+              <li><Link to="/about">Acerca de Nosotros</Link></li>
+            </ul>
+          </div>
+          <Routes>
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Router>
       </header>
     </div>
   );
